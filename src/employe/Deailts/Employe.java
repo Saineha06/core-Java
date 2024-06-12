e2b9617f545d0a006b40d215140192ce6e6daccf
saineha <sainehamarru27@gmail.com>
2024-06-12 13:08:36 +0530

    Core java

---
+++ src/employe/Deailts/Employe.java
@@ -0,0 +1,16 @@
+package employe.Deailts;
+
+public class Employe {
+    public String empName;
+    public int empAge;
+    public String empId;
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "empName='" + empName + '\'' +
+                ", empAge=" + empAge +
+                ", empId='" + empId + '\'' +
+                '}';
+    }
+}
